bd43ed567f8c61cd8611bbdf521e58e7ce4538de
dinhsangtr <sangtruong2811@gmail.com>
2023-03-17 02:55:59 +0700

    refactor code

--- src/routes/botChatwork.ts
+++ src/routes/botChatwork.ts
@@ -71,54 +71,66 @@ routes.post('/', async (req, res) => {
           question = splitMsg[0];
         }
         console.log('Question:', question);
-        const responseGPT = await axios.post(
-          'https://api.openai.com/v1/chat/completions',
-          {
-            // prompt: question,
-            // temperature: 0.5,
-            // max_tokens: process.env.GPT_MAX_TOKEN || 100,
-            // top_p: 1,
-            // frequency_penalty: 1,
-            // presence_penalty: 0.5,
-            model: 'gpt-3.5-turbo',
-            messages: [{ role: 'user', content: question }],
-            temperature: 0.6,
-            max_tokens: process.env.GPT_MAX_TOKEN || 100,
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${process.env.GPT_API_KEY}`,
+        await axios
+          .post(
+            'https://api.openai.com/v1/chat/completions',
+            {
+              // prompt: question,
+              // temperature: 0.5,
+              // max_tokens: process.env.GPT_MAX_TOKEN || 100,
+              // top_p: 1,
+              // frequency_penalty: 1,
+              // presence_penalty: 0.5,
+              model: 'gpt-3.5-turbo',
+              messages: [{ role: 'user', content: question }],
+              temperature: 0.5,
+              max_tokens: process.env.GPT_MAX_TOKEN || 100,
             },
-          }
-        );
-        const answer = responseGPT.data.choices[0].message.content;
-        console.log('Answer:', answer);
-        // Handle send chatwork
-        // Set up the Axios instance with the API endpoint and access token in the headers
-        const chatworkApi = axios.create({
-          baseURL: baseUrl,
-          headers: {
-            'X-ChatWorkToken': accessToken,
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'ChatWork-Webhook/1.0.0',
-            'x-chatworkwebhooksignature': process.env.SIGNATURE,
-          },
-        });
-        // Make a call to the Chatwork API to send the message
-        await chatworkApi
-          .post(`/rooms/${roomId}/messages`, {
-            body: `[To:${fromAccountId}]${answer}`,
-          })
-          .then((response) => {
-            return res.status(200).json({
-              msg: 'OK',
-              data: response.data,
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.GPT_API_KEY}`,
+              },
+            }
+          )
+          .then(async (responseGPT) => {
+            const answer = responseGPT.data.choices[0].message.content;
+            console.log('Answer:', answer);
+            // Handle send chatwork
+            // Set up the Axios instance with the API endpoint and access token in the headers
+            const chatworkApi = axios.create({
+              baseURL: baseUrl,
+              headers: {
+                'X-ChatWorkToken': accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'ChatWork-Webhook/1.0.0',
+                'x-chatworkwebhooksignature': process.env.SIGNATURE,
+              },
             });
+            // Make a call to the Chatwork API to send the message
+            await chatworkApi
+              .post(`/rooms/${roomId}/messages`, {
+                body: `[To:${fromAccountId}]${answer}`,
+              })
+              .then((response) => {
+                return res.status(200).json({
+                  msg: 'OK',
+                  data: response.data,
+                });
+              })
+              .catch((error) => {
+                console.log('ERROR CHATWORK', error)
+                return res.status(500).json({
+                  msg: 'ERROR CHATWORK',
+                  data: null,
+                  error: error,
+                });
+              });
           })
           .catch((error) => {
+            console.log('ERROR GPT', error)
             return res.status(500).json({
-              msg: 'ERROR',
+              msg: 'ERROR GPT',
               data: null,
               error: error,
             });
