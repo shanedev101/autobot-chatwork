9ea3e1a545ab3651b260a84ffade625e766f5137
dinhsangtr <sangtruong2811@gmail.com>
2023-03-17 00:33:38 +0700

    bot-chatwork

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-import "./lib/db";
-import express from "express";
-import countryRoutes from "./routes/country";
+// import "./lib/db";
+import botChatworkRoutes from './routes/botChatwork';
+import express from 'express';
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3333;
+const app_url = process.env.APP_URL || 'http://localhost';
 
 app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
 
-app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+app.get('/', async (req, res) => {
+  res.json({ message: 'Hello world!' });
 });
 
-app.use("/countries", countryRoutes);
+app.use('/bot-chatwork', botChatworkRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at ${app_url}:${port}`);
 });
---
+++ src/routes/botChatwork.ts
@@ -0,0 +1,99 @@
+import { Router } from 'express';
+import axios from 'axios';
+const dotenv = require('dotenv');
+dotenv.config();
+
+const routes = Router();
+
+// Set up the API endpoint and access token
+const baseUrl = 'https://api.chatwork.com/v2';
+const accessToken = process.env.CHATWORK_TOKEN || '';
+
+routes.get('/', async (req, res) => {
+  try {
+    if (!accessToken) {
+      return res.status(500).json({ msg: 'Missing chatwork token' });
+    }
+
+    // Set up the Axios instance with the API endpoint and access token in the headers
+    const chatworkApi = axios.create({
+      baseURL: baseUrl,
+      headers: {
+        'X-ChatWorkToken': accessToken,
+      },
+    });
+
+    // Make a call to the Chatwork API to get the list of rooms
+    await chatworkApi
+      .get('/rooms')
+      .then((response) => {
+        return res.status(200).json({
+          msg: 'OK',
+          data: response.data,
+        });
+      })
+      .catch((error) => {
+        return res.status(500).json({
+          msg: 'ERROR',
+          data: null,
+          error: error,
+        });
+      });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Sorry, something went wrong :/' });
+  }
+});
+
+routes.post('/', async (req, res) => {
+  try {
+    if (!accessToken) {
+      return res.status(500).json({ msg: 'Missing chatwork token' });
+    }
+
+    let roomId = '',
+      message = '',
+      fromAccountId = '';
+    if (req.body) {
+      roomId = req.body.room_id;
+      message = req.body.webhook_event.body;
+      fromAccountId = req.body.webhook_event.from_account_id;
+      if (roomId && message && fromAccountId) {
+        // Set up the Axios instance with the API endpoint and access token in the headers
+        const chatworkApi = axios.create({
+          baseURL: baseUrl,
+          headers: {
+            'X-ChatWorkToken': accessToken,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
+        // Make a call to the Chatwork API to send the message
+        await chatworkApi
+          .post(`/rooms/${roomId}/messages`, {
+            body: `[To:${fromAccountId}]` + message,
+          })
+          .then((response) => {
+            return res.status(200).json({
+              msg: 'OK',
+              data: response.data,
+            });
+          })
+          .catch((error) => {
+            return res.status(500).json({
+              msg: 'ERROR',
+              data: null,
+              error: error,
+            });
+          });
+      }
+    } else {
+      return res.status(400).json({ msg: 'Missing data. Skip send.' });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: `Sorry, something went wrong :/\n${error}` });
+  }
+});
+
+export default routes;
